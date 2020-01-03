Try numpy linalg fixed scaling matrix to use stdev

cma_es.py
@@ -193,8 +193,8 @@ if __name__ == '__main__':
         # make matrix positive definite or symeig will not like it
         # c = torch.from_numpy(np_nearestPD(c.cpu().numpy())).to(args.device)
 
-        #d, b = c.symeig(True)
-        d, b = la.eigh(c.cpu().numpy())
+        d, b = c.symeig(True)
+        #d, b = la.eigh(c.cpu().numpy())
         d, b = torch.from_numpy(d).to(args.device), torch.from_numpy(b).to(args.device)
         # check this is correct
         d = d.sqrt().diag_embed()