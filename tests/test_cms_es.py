More scaling fixes

tests/test_cms_es.py
@@ -60,7 +60,7 @@ def plot_heatmap(title, count, mean, b, d, samples=None, g=None):
     unit = torch.cat((unit, torch.ones(2, 1, 2)), dim=1)
     unit = trans.matmul(unit)
 
-    ax2.scatter(mean[0], mean[1], color='green')
+    ax2.scatter(mean[0], mean[1], color='yellow')
 
     xunit_x, xunit_y = unit[0, 0], unit[0, 1]
     yunit_x, yunit_y = unit[1, 0], unit[1, 1]
@@ -73,9 +73,11 @@ def plot_heatmap(title, count, mean, b, d, samples=None, g=None):
 
     max_g_x = g[:, 0].abs().max() if g is not None else 0
     max_g_y = g[:, 1].abs().max() if g is not None else 0
+    max_s_x = samples[:, 0].abs().max() if samples is not None else 0
+    max_s_y = samples[:, 1].abs().max() if samples is not None else 0
 
-    xscale = max(xunit_x.abs().max().item(), yunit_x.abs().max().item(), 0.3, max_g_x) * 1.1
-    yscale = max(xunit_y.abs().max().item(), yunit_y.abs().max().item(), 0.4, max_g_y) * 1.1
+    xscale = max(xunit_x.abs().max().item(), yunit_x.abs().max().item(), 0.3, max_g_x, max_s_x) * 1.1
+    yscale = max(xunit_y.abs().max().item(), yunit_y.abs().max().item(), 0.4, max_g_y, max_s_y) * 1.1
 
     ax2.set_xlim(-xscale + mean[0], xscale + mean[0])
     ax2.set_ylim(-yscale + mean[1], yscale + mean[1])
@@ -104,12 +106,18 @@ def test_sampler():
     plt.show()
 
     mean = torch.zeros(features)
-    c = torch.eye(features)
     b = torch.eye(features)
     d = torch.eye(features)
+    c = torch.matmul(b.matmul(d), b.matmul(d).T)
+
+    print(c)
 
     for counteval in range(4):
+
+        # sample parameters
         s, z = cma_es.sample(samples, step_size, mean, b, d)
+
+        # rank by fitness
         f = objective(s[:, 0], s[:, 1])
         g = [{'sample': s[i], 'fitness':f.item()} for i, f in enumerate(f)]
         g = sorted(g, key=lambda x: x['fitness'], reverse=True)
@@ -118,10 +126,14 @@ def test_sampler():
         plot_heatmap('sample ', counteval, mean, b, d, samples=s, g=g)
         mean_prev = mean.clone()
         c_prev = c.clone()
+
+        xmean = g.mean() * weights
+        zmean = z.mean() * weights
+
         g_raw = g.clone()
         mean = g.mean(0)
         g = g - mean_prev
-        c = g.T.matmul(g) * 4 / samples
+        c = g.T.matmul(g) / mu
         c = (1.0 - cmu) * c_prev + cmu * c
         d, b = torch.symeig(c, eigenvectors=True)
         d = d.sqrt().diag_embed()